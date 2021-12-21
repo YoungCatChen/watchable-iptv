3b7e47539af2854e3829fba8197186c939eb64a4
YoungCatChen <chxuexuan@gmail.com>
2021-12-21 00:34:38 -0800

    Ability to parse an m3u8 file.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
-const WORLD = 'world';
+import * as fs from 'fs';
+import * as m3u8Parser from 'm3u8-parser';
+import {Manifest} from './m3u8-parser-objects';
 
-/** */
-export function hello(world: string = WORLD): string {
-  return `Hello ${world}! `;
-}
+m3u8Parser.A;
+console.log('Will read this file:');
+console.log(process.argv[2]);
+const contents = fs.readFileSync(process.argv[2], {encoding: 'utf-8'});
+console.log('Done reading the file. Length: ', contents.length, 'Will parse.');
+const parser = new m3u8Parser.Parser();
+parser.push(contents);
+parser.end();
+const manifest = parser.manifest as Manifest;
+console.log('Done parsing. Results:');
+console.log(JSON.stringify(manifest, null, '  '));
---
+++ src/m3u8-parser-objects.ts
@@ -0,0 +1,64 @@
+export interface Manifest {
+  allowCache: boolean;
+  endList: boolean;
+  mediaSequence: number;
+  discontinuitySequence: number;
+  playlistType: string;
+  custom: {};
+  playlists: Array<{
+    attributes: {};
+    // Manifest
+  }>;
+  mediaGroups: {
+    AUDIO: {
+      'GROUP-ID': {
+        NAME: {
+          default: boolean;
+          autoselect: boolean;
+          language: string;
+          uri: string;
+          instreamId: string;
+          characteristics: string;
+          forced: boolean;
+        };
+      };
+    };
+    VIDEO: {};
+    'CLOSED-CAPTIONS': {};
+    SUBTITLES: {};
+  };
+  dateTimeString: string;
+  dateTimeObject: Date;
+  targetDuration: number;
+  totalDuration: number;
+  discontinuityStarts: number[];
+  segments: Segment[];
+}
+
+export interface Segment {
+  byterange: {
+    length: number;
+    offset: number;
+  };
+  duration: number;
+  attributes: {};
+  discontinuity: number;
+  uri: string;
+  timeline: number;
+  key: {
+    method: string;
+    uri: string;
+    iv: string;
+  };
+  map: {
+    uri: string;
+    byterange: {
+      length: number;
+      offset: number;
+    };
+  };
+  'cue-out': string;
+  'cue-out-cont': string;
+  'cue-in': string;
+  custom: {};
+}
