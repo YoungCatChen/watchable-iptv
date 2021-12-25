e4c92ad18db32c91d4b5362758e7f72084c60de1
YoungCatChen <chxuexuan@gmail.com>
2021-12-24 21:33:07 -0800

    Ability to parse an m3u8 channel list.

---
+++ src/m3u8-channel-list.ts
@@ -0,0 +1,60 @@
+import {URL} from 'url';
+
+export interface M3u8ChannelList {
+  headerText: string;
+  channels: M3u8Channel[];
+}
+
+export interface M3u8Channel {
+  text: string;
+  url?: URL;
+}
+
+export function parseM3u8ChannelList(
+  text: string,
+  channelListUrl: string | URL
+): M3u8ChannelList {
+  let isHeader = true;
+  const headerLines: string[] = [];
+
+  let currentChannelLines: string[] = [];
+  let currentChannelUrl: URL | undefined = undefined;
+  const channels: M3u8Channel[] = [];
+
+  function finalizeChannel() {
+    if (currentChannelLines.length !== 0) {
+      channels.push({
+        text: currentChannelLines.join('\n'),
+        url: currentChannelUrl,
+      });
+    }
+    currentChannelLines = [];
+    currentChannelUrl = undefined;
+  }
+
+  for (let line of text.split(/\r?\n/)) {
+    line = line.trim();
+    if (line.length === 0) continue;
+    if (line.startsWith('#EXTINF:')) {
+      isHeader = false;
+      finalizeChannel();
+    }
+    if (isHeader) {
+      headerLines.push(line);
+    } else {
+      currentChannelLines.push(line);
+      if (!line.startsWith('#')) {
+        currentChannelUrl = new URL(line, channelListUrl);
+      }
+    }
+  }
+
+  finalizeChannel();
+  return {headerText: headerLines.join('\n'), channels};
+}
+
+// import * as fs from 'fs';
+// console.log('Will read this file:', process.argv[2]);
+// const contents = fs.readFileSync(process.argv[2], {encoding: 'utf-8'});
+// const results = parseM3u8ChannelList(contents, 'http://a/b/c');
+// console.log(results);
