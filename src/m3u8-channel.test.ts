6dd2f04e8ebcd4111a292351b493fdfd8828f552
YoungCat CHEN <YoungCatChen@users.noreply.github.com>
2022-01-09 23:12:11 -0800

    Completed tests for M3u8Channel.

--- src/m3u8-channel.test.ts
+++ src/m3u8-channel.test.ts
@@ -1,8 +1,9 @@
 import {M3u8Channel} from './m3u8-channel.js';
 import {Mock} from 'jest-mock';
 import {jest} from '@jest/globals';
+import {ChannelProbeResult} from './channel-prober.js';
 
-describe('M3u8Channel.consumeAndParse', () => {
+describe('M3u8Channel.consumeAndParse()', () => {
   function mockCtor(): typeof M3u8Channel & Mock<unknown, unknown[]> {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     return jest.fn() as any;
@@ -81,5 +82,82 @@ describe('M3u8Channel.consumeAndParse', () => {
     expect(ctor).toHaveBeenCalledWith(['#AA', '#EXTINF:', 'http://a', '#BB']);
     expect(result).toBeTruthy();
     expect(arr).toEqual(['#EXTINF:', 'http://b']);
+  });
+});
+
+describe('M3u8Channel constructor', () => {
+  class TestChannel extends M3u8Channel {
+    get textPattern() {
+      return this.textPattern_;
+    }
+  }
+
+  it('gets information from text', () => {
+    const ch = new TestChannel(['#EXTINF:0 group-title="A",BTV-1', 'http://a']);
+    expect(ch.channelName).toEqual('BTV-1');
+    expect(ch.channelGroup).toEqual('A');
+    expect(ch.url).toEqual('http://a');
+    expect(ch.textPattern).toEqual(
+      '#EXTINF:0 group-title="{{GROUP}}",{{NAME}}\n{{URL}}'
+    );
+  });
+
+  it('respects empty channel name and empty channel group', () => {
+    const ch = new TestChannel(['#EXTINF:0 group-title="",', 'http://a']);
+    expect(ch.channelName).toEqual('');
+    expect(ch.channelGroup).toEqual('');
+    expect(ch.textPattern).toEqual(
+      '#EXTINF:0 group-title="{{GROUP}}",{{NAME}}\n{{URL}}'
+    );
+  });
+
+  it('gets information from #EXT-X-STREAM-INF directive too', () => {
+    const ch = new TestChannel([
+      '#EXT-X-STREAM-INF:group-title="A",BTV-1',
+      'http://a',
+    ]);
+    expect(ch.channelName).toEqual('BTV-1');
+    expect(ch.channelGroup).toEqual('A');
+  });
+
+  it('does not over-extract from wrong directive', () => {
+    const ch = new TestChannel(['#WRONG:0 group-title="A",BTV-1', 'http://a']);
+    expect(ch.channelName).toEqual('');
+    expect(ch.channelGroup).toEqual('');
+  });
+
+  it('does not over-extract channel name from random attribute', () => {
+    const ch = new TestChannel(['#EXT-X-STREAM-INF:FRAME-RATE=30', 'http://a']);
+    expect(ch.channelName).toEqual('');
+  });
+
+  it('trims channel name', () => {
+    const ch = new TestChannel(['#EXTINF:0, BTV-1', 'http://a']);
+    expect(ch.channelName).toEqual('BTV-1');
+    expect(ch.textPattern).toEqual('#EXTINF:0,{{NAME}}\n{{URL}}');
+  });
+
+  it('passes text through', () => {
+    const ch = new TestChannel(['#EXTINF:0,BTV-1', '#FOO', 'http://a', '#BAR']);
+    expect(ch.textPattern).toEqual('#EXTINF:0,{{NAME}}\n#FOO\n{{URL}}\n#BAR');
+  });
+});
+
+describe('M3u8Channel.composeText()', () => {
+  it('overwrites channel name and channel group', () => {
+    const ch = new M3u8Channel(['#EXTINF:0 group-title="A",BTV-1', 'http://a']);
+    const text = ch.composeText({channelName: '@@', channelGroup: '##'});
+    expect(text).toEqual('#EXTINF:0 group-title="##",@@\nhttp://a');
+  });
+});
+
+describe('M3u8Channel.composeText() and fillInProbeResult()', () => {
+  it('respect dereferenced url', () => {
+    const probeResult = new ChannelProbeResult();
+    jest.spyOn(probeResult, 'getDereferencedUrl').mockReturnValue('http://b');
+    const ch = new M3u8Channel(['#EXTINF:0,BTV-1', 'http://a']);
+    ch.fillInProbeResult(probeResult);
+    const text = ch.composeText({useDereferencedUrl: true});
+    expect(text).toEqual('#EXTINF:0,BTV-1\nhttp://b');
   });
 });
--- src/m3u8-channel.ts
+++ src/m3u8-channel.ts
@@ -1,4 +1,5 @@
 // import Deque from 'collections/deque.js';
+import assert from 'assert';
 import {ChannelProbeResult} from './channel-prober';
 
 export interface ChannelTextComposeOptions {
@@ -29,26 +30,22 @@ export class M3u8Channel {
     return channel;
   }
 
-  private readonly textPattern: string;
-  private readonly channelName: string;
-  private readonly channelGroup: string;
-  private readonly url: string;
+  protected readonly textPattern_: string;
+  private channelName_ = '';
+  private channelGroup_ = '';
+  private url_ = '';
   private probePassed_?: boolean;
   private dereferencedUrl_?: string;
 
-  constructor(lines: string[]) {
-    this.textPattern = '';
-    this.channelName = '';
-    this.channelGroup = '';
-    this.url = '';
-    console.log('real constructor');
+  get channelName() {
+    return this.channelName_;
   }
-
-  fillInProbeResult(probeResult: ChannelProbeResult): void {
-    this.probePassed_ = probeResult.passed;
-    this.dereferencedUrl_ = probeResult.getDereferencedUrl();
+  get channelGroup() {
+    return this.channelGroup_;
   }
-
+  get url() {
+    return this.url_;
+  }
   get probePassed() {
     return this.probePassed_;
   }
@@ -56,13 +53,52 @@ export class M3u8Channel {
     return this.dereferencedUrl_;
   }
 
+  constructor(
+    /**
+     * Lines of texts. Caller must assure:
+     *
+     * 1. each line to be `trim()`ed, otherwise `this.url` would be wrong.
+     * 2. a URL line to be included, otherwise an exception would be thrown.
+     * 3. that they only include one URL line, and no more than one media-start
+     *    line, otherwise the information from last occurrence would be
+     *    captured and information from other occurrence(s) would be lost, and
+     *    `composeText()` wouldn't work correctly.
+     */
+    lines: string[]
+  ) {
+    for (let i = 0; i < lines.length; i++) {
+      if (!lines[i].startsWith('#')) {
+        this.url_ = lines[i];
+        lines[i] = '{{URL}}';
+      }
+      if (isMediaStart(lines[i])) {
+        lines[i] = lines[i]
+          .replace(/\b(group-title=['"])(.*?)(['"])/, (match, p1, p2, p3) => {
+            this.channelGroup_ = p2;
+            return p1 + '{{GROUP}}' + p3;
+          })
+          .replace(/,([^,=]*)$/, (match, p1) => {
+            this.channelName_ = p1.trim();
+            return ',{{NAME}}';
+          });
+      }
+    }
+    assert.ok(this.url_);
+    this.textPattern_ = lines.join('\n');
+  }
+
+  fillInProbeResult(probeResult: ChannelProbeResult): void {
+    this.probePassed_ = probeResult.passed;
+    this.dereferencedUrl_ = probeResult.getDereferencedUrl();
+  }
+
   composeText(options?: ChannelTextComposeOptions) {
     const url =
-      (options?.useDereferencedUrl && this.dereferencedUrl_) || this.url || '';
-    return this.textPattern
+      (options?.useDereferencedUrl && this.dereferencedUrl_) || this.url_ || '';
+    return this.textPattern_
       .replace('{{URL}}', url)
-      .replace('{{NAME}}', options?.channelName || this.channelName)
-      .replace('{{GROUP}}', options?.channelGroup || this.channelGroup);
+      .replace('{{NAME}}', options?.channelName || this.channelName_)
+      .replace('{{GROUP}}', options?.channelGroup || this.channelGroup_);
   }
 }
 
