9f1b9461f7b642854f4158cb46662b76c35596c8
YoungCat CHEN <YoungCatChen@users.noreply.github.com>
2022-01-16 13:13:35 -0800

    Added test cases for redirection.

--- src/downloader.test.ts
+++ src/downloader.test.ts
@@ -8,26 +8,39 @@ import {
   RequestListener,
 } from 'http';
 import {lastValueFrom} from 'rxjs';
+import {URL} from 'url';
 import {download} from './downloader';
 
 interface ServerInfo {
   readonly serverInstance: Server;
   readonly port: number;
-  readonly handlers: Map<string, RequestListener>;
+  readonly listeners: Map<string, RequestListener>;
 }
 
 function startServer(): Promise<ServerInfo> {
-  const handlers = new Map<string, RequestListener>();
+  const listeners = new Map<string, RequestListener>();
 
   function globalRequestListener(req: IncomingMessage, resp: ServerResponse) {
-    for (const [path, listener] of handlers.entries()) {
-      if (path === req.url) {
+    assert.ok(req.url);
+    const url = new URL(req.url, 'http://dummy');
+
+    if (url.pathname === '/redirect') {
+      const destination = url.searchParams.get('url');
+      assert.ok(destination);
+      resp.writeHead(302, {['Location']: destination});
+      resp.end();
+      return;
+    }
+
+    for (const [path, listener] of listeners.entries()) {
+      if (path === url.pathname) {
         listener(req, resp);
         return;
       }
     }
-    resp.writeHead(404, 'Listener not found');
-    resp.end('Listener not found!');
+
+    resp.writeHead(404, `Listener for ${req.url} is not found`);
+    resp.end(`Listener for ${req.url} is not found!`);
   }
 
   return new Promise(resolve => {
@@ -36,62 +49,75 @@ function startServer(): Promise<ServerInfo> {
       const address = server.address();
       assert.ok(address);
       assert.ok(typeof address === 'object');
-      resolve({serverInstance: server, port: address.port, handlers});
+      resolve({serverInstance: server, port: address.port, listeners});
     });
   });
 }
 
 describe('download()', () => {
   let serverInfo: ServerInfo | null = null;
-  let hostport = 'http://1.1.1.1:1111';
 
   beforeAll(async () => {
     serverInfo = await startServer();
-    hostport = `http://localhost:${serverInfo.port}`;
   });
 
   afterEach(() => {
-    serverInfo?.handlers.clear();
+    serverInfo?.listeners.clear();
   });
 
   afterAll(() => {
     serverInfo?.serverInstance.close();
   });
 
-  it('downloads a text file', async () => {
-    serverInfo!.handlers.set('/text', (req, resp) => {
-      resp.writeHead(200);
-      resp.end('body');
-    });
-    const dr = await lastValueFrom(download(`${hostport}/text`, 1));
-    expect(dr.status).toBe('done');
-    expect(dr.reqUrl.href).toContain('/text');
-    expect(dr.respUrl?.href).toContain('/text');
-    expect(dr.statusCode).toBe(200);
-    expect(dr.error).toBeFalsy();
-    expect(dr.byteLength).toBe(4);
-    expect(dr.looksLikeText).toBeTruthy();
-    expect(dr.text).toBe('body');
-  });
+  describe.each`
+    redirected | description
+    ${false}   | ${'(when response is direct)'}
+    ${true}    | ${'(when response is redirected)'}
+  `('$description', ({redirected}) => {
+    let urlPrefix = '';
 
-  it('downloads a binary file', async () => {
-    serverInfo!.handlers.set('/binary', (req, resp) => {
-      resp.writeHead(200);
-      resp.end(Buffer.from([0, 0, 0, 0x1c, 0x66, 0x74, 0x79, 0x70]));
+    beforeAll(() => {
+      if (redirected) {
+        urlPrefix = `http://localhost:${serverInfo!.port}/redirect?url=`;
+      } else {
+        urlPrefix = `http://localhost:${serverInfo!.port}`;
+      }
     });
-    const dr = await lastValueFrom(download(`${hostport}/binary`, 1));
-    expect(dr.status).toBe('done');
-    expect(dr.byteLength).toBe(8);
-    expect(dr.looksLikeText).toBeFalsy();
-  });
 
-  xit('tracks download speed', async () => {});
-  xit('follows redirection', async () => {});
-  xit('reports error on unsupported scheme', async () => {});
-  xit('reports error on 404 not found', async () => {});
-  xit('reports error on connection lost', async () => {});
-  xit('reports time-out before initial response comes', async () => {});
-  xit('reports time-out before redirected response comes', async () => {});
-  xit('reports time-out before response ends', async () => {});
-  xit('reports aborted on unsubscribed observable', async () => {});
+    it('downloads a text file', async () => {
+      serverInfo!.listeners.set('/text', (req, resp) => {
+        resp.writeHead(200).end('body');
+      });
+      const dr = await lastValueFrom(download(`${urlPrefix}/text`, 1));
+      expect(dr.status).toBe('done');
+      expect(dr.reqUrl.href).toContain('/text');
+      expect(dr.respUrl?.href).toContain('/text');
+      expect(dr.statusCode).toBe(200);
+      expect(dr.error).toBeFalsy();
+      expect(dr.byteLength).toBe(4);
+      expect(dr.looksLikeText).toBeTruthy();
+      expect(dr.text).toBe('body');
+    });
+
+    it('downloads a binary file', async () => {
+      serverInfo!.listeners.set('/binary', (req, resp) => {
+        resp.writeHead(200);
+        resp.end(Buffer.from([0, 0, 0, 0x1c, 0x66, 0x74, 0x79, 0x70]));
+      });
+      const dr = await lastValueFrom(download(`${urlPrefix}/binary`, 1));
+      expect(dr.status).toBe('done');
+      expect(dr.byteLength).toBe(8);
+      expect(dr.looksLikeText).toBeFalsy();
+    });
+
+    xit('tracks download speed', async () => {});
+    xit('follows redirection', async () => {});
+    xit('reports error on unsupported scheme', async () => {});
+    xit('reports error on connection establishment', async () => {});
+    xit('reports error on bad status code', async () => {});
+    xit('reports error on connection lost', async () => {});
+    xit('reports time-out on response late on first byte', async () => {});
+    xit('reports time-out on response body taking too long', async () => {});
+    xit('reports aborted on unsubscribed observable', async () => {});
+  });
 });
